07a5325f2d0b040a05bc5b84c2d7d0c5fb5f96ac
carlolic <carlo.licciulli@adwmainz.de>
2025-04-24 17:21:27 +0200

    add test shoelace button

--- src/themes/mermeid.ts
+++ src/themes/mermeid.ts
@@ -4,6 +4,7 @@ import { Editor, InputListEntry, Theme } from "../theme"
 import { PREFIX_XSD } from '../constants'
 import { Literal } from 'n3'
 import css from './mermeid.css?raw'
+import "https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.17.1/cdn/shoelace.js"
 
 export class MermeidTheme extends Theme {
     idCtr = 0
@@ -52,6 +53,10 @@ export class MermeidTheme extends Theme {
         const result = document.createElement('div')
         result.appendChild(labelElem)
         result.appendChild(editor)
+
+        const iconButton = this.createIconButton("plus-circle", true, "add-icon-button")
+        result.appendChild(iconButton)
+
         return result
     }
 
@@ -236,5 +241,14 @@ export class MermeidTheme extends Theme {
         button.type = 'button'
         button.innerHTML = label
         return button
+    }
+
+    createIconButton(iconName: string, primary: boolean, cssClass?: string): HTMLElement {
+        const iconButton = document.createElement('sl-icon-button')
+        iconButton.setAttribute('name', iconName)
+        if (cssClass) {
+            iconButton.classList.add(cssClass)
+        }
+        return iconButton
     }
 }
